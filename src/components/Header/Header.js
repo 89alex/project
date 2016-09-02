856d779e30122e69a2636c01554769826498f0c5
89alex <593536876@qq.com>
2016-09-02 16:56:30 +0800

    9.2

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { IndexLink, Link } from 'react-router'
+import classes from './Header.scss'
+
+export const Header = () => (
+  <div>
+    <h1>React Starter Kit ---- <a href='https://bodyno.com' target='_blank'>bodyno</a></h1>
+    <IndexLink to='/' activeClassName={classes.activeRoute}>
+      Home
+    </IndexLink>
+    {' · '}
+    <Link to='/counter' activeClassName={classes.activeRoute}>
+      Counter
+    </Link>
+    {' · '}
+    <Link to='/zen' activeClassName={classes.activeRoute}>
+      Zen
+    </Link>
+    {' · '}
+    <Link to='/elapse' activeClassName={classes.activeRoute}>
+      Elapse
+    </Link>
+    {' · '}
+    <Link to='/route/88' activeClassName={classes.activeRoute}>
+      Route
+    </Link>
+    {' · '}
+    <Link to='/notFound' activeClassName={classes.activeRoute}>
+      404
+    </Link>
+  </div>
+)
+
+export default Header
